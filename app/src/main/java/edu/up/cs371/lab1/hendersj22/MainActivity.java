e3bfc4dfa3b6349fe52dce6543f082d9d2da8c86
Joshua Henderson <hendersj22@up.edu>
2020-01-13 20:03:08 -0800

    Initial Commit

---
+++ app/src/main/java/edu/up/cs371/lab1/hendersj22/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.up.cs371.lab1.hendersj22;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
